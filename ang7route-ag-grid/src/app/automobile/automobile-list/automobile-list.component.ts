85e4dc19fcf7f186ff3595048fd02da3758130ad
max cheung <maxcheung@optusnet.com.au>
2019-03-10 09:34:13 +1100

    planes trains and automobiles

--- ang7route-ag-grid/src/app/automobile/automobile-list/automobile-list.component.ts
+++ ang7route-ag-grid/src/app/automobile/automobile-list/automobile-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AgGridNg2 } from 'ag-grid-angular';
+
 
 @Component({
   selector: 'app-automobile-list',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./automobile-list.component.scss']
 })
 export class AutomobileListComponent implements OnInit {
+  @ViewChild('agGrid') agGrid: AgGridNg2;
+  title = 'app';
 
-  constructor() { }
+  columnDefs = [
+    {headerName: 'Make', field: 'make', sortable: true, filter: true},
+    {headerName: 'Model', field: 'model', sortable: true, filter: true},
+    {headerName: 'Price', field: 'price', sortable: true, filter: true}
+];
 
-  ngOnInit() {
-  }
+rowData: any;
+
+constructor(private http: HttpClient) {
+
+}
+
+ngOnInit() {
+  this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
+}
+
+getSelectedRows() {
+  const selectedNodes = this.agGrid.api.getSelectedNodes();
+  const selectedData = selectedNodes.map( node => node.data );
+  const selectedDataStringPresentation = selectedData.map( node => node.make + ' ' + node.model).join(', ');
+  alert(`Selected nodes: ${selectedDataStringPresentation}`);
+}
 
 }
--- ang7route-ag-grid/src/app/automobile/automobile.module.ts
+++ ang7route-ag-grid/src/app/automobile/automobile.module.ts
@@ -8,11 +8,14 @@ import { AutomobileRoutingModule } from './automobile-routing.module';
 import { AutomobileComponent } from './automobile/automobile.component';
 import { AutomobileListComponent } from './automobile-list/automobile-list.component';
 import { AutomobileDetailComponent } from './automobile-detail/automobile-detail.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AutomobileComponent, AutomobileListComponent, AutomobileDetailComponent],
   imports: [
-    CommonModule,
+    CommonModule, HttpClientModule, 
+    AgGridModule.withComponents([]),
     AutomobileRoutingModule
   ]
 })
