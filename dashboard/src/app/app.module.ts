5611c1f8e39a0717707dcd969c0bcaca93fb2f18
max cheung <maxcheung@optusnet.com.au>
2019-03-10 15:23:05 +1100

    dashboard

---
+++ dashboard/src/app/app.module.ts
@@ -0,0 +1,29 @@
+// app.module.ts
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { StudentModule } from './student/student.module';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardpanelComponent } from './shared/components/dashboardpanel/dashboardpanel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    DashboardpanelComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StudentModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
